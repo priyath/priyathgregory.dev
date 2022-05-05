e0089e25160f04366f232781da3486bdc72056ae
priyath <priyath.gregory@gmail.com>
2022-05-05 08:17:56 +0530

    updated next configs for mdx

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
-const withTM = require('next-transpile-modules')(
-    ['@mui/material', '@mui/system']
-); // pass the modules you would like to see transpiled
+const withPlugins = require('next-compose-plugins');
+const withTM = require('next-transpile-modules')([
+  '@mui/material',
+  '@mui/system',
+]); // pass the modules you would like to see transpiled
+const withMDX = require('@next/mdx')();
 
-module.exports = withTM({
-  reactStrictMode: true,
-});
+// optional next.js configuration
+const nextConfig = {};
+
+module.exports = withPlugins(
+  [
+    // add a plugin with specific configuration
+    [
+      withTM,
+      {
+        reactStrictMode: true,
+      },
+    ],
+    [withMDX, { pageExtensions: ['js', 'mdx'] }],
+  ],
+  nextConfig
+);
