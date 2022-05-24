22ab97c3b1d806a1a60f714216a5b754413d77a4
priyath <priyath.gregory@gmail.com>
2022-05-24 14:57:54 +0530

    content styling minor fixes

--- components/MDXComponents/BlogList/BlogListItem/index.tsx
+++ components/MDXComponents/BlogList/BlogListItem/index.tsx
@@ -2,8 +2,21 @@ import * as React from 'react';
 import { ListItem } from '@mui/material';
 
 const BlogListItem = (props: any) => {
+  console.log('list item: ', props);
   return (
-    <ListItem sx={{ display: 'list-item', py: 0, mb: 2 }}>
+    <ListItem
+      sx={{
+        display: 'list-item',
+        py: 0,
+        mb: 2,
+        '&:last-child': {
+          mb: {
+            xs: 3,
+            md: 4,
+          },
+        },
+      }}
+    >
       {props.children}
     </ListItem>
   );
