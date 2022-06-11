eea43ccf8c94e8eb2a9f6644bd3c010d9e09de01
priyath <priyath.gregory@gmail.com>
2022-06-11 10:53:09 +0800

    code block styling, collapsable code block support

--- components/MDXComponents/CodeBlock/index.tsx
+++ components/MDXComponents/CodeBlock/index.tsx
@@ -1,8 +1,53 @@
-import { Box } from '@mui/material';
+import { AccordionSummaryProps, Box, styled, Typography } from '@mui/material';
+import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
+import MuiAccordionSummary from '@mui/material/AccordionSummary';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { extractMDXLanguage } from '../utils';
+import { extractMDXTags } from '../utils';
 import dark from 'react-syntax-highlighter/dist/esm/styles/prism/atom-dark';
+// import light from 'react-syntax-highlighter/dist/esm/styles/prism/one-light';
 import * as React from 'react';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+
+const Accordion = styled((props: AccordionProps) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  '&:not(:last-child)': {
+    borderBottom: 0,
+  },
+  '&:before': {
+    display: 'none',
+  },
+  borderRadius: '5px',
+}));
+
+const AccordionSummary = styled((props: AccordionSummaryProps) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    {...props}
+    sx={{ margin: 0, borderColor: 'red' }}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === 'dark'
+      ? 'rgba(255, 255, 255, .05)'
+      : 'rgba(0, 0, 0, .03)',
+  flexDirection: 'row-reverse',
+  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+    transform: 'rotate(90deg)',
+  },
+  '& .MuiAccordionSummary-content': {
+    marginLeft: theme.spacing(1),
+    borderColor: 'red',
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: 0,
+  borderTop: '1px solid rgba(0, 0, 0, .125)',
+}));
 
 interface ICodeBlock {
   children: {
@@ -19,23 +64,48 @@ const CodeBlock = ({ children }: ICodeBlock) => {
     props: { className, children: code = '' },
   } = children;
 
+  const mdxTags = extractMDXTags(className);
+
   return (
     <Box
       sx={{
+        fontSize: 14,
         marginY: { xs: 3, md: 4 },
         width: {
           sm: '100%',
           xs: 'calc(100vw - 48px)',
+          maxHeight: '95vh',
+          overflow: 'auto',
         },
       }}
     >
-      <SyntaxHighlighter
-        wrapLongLines={true}
-        language={extractMDXLanguage(className)}
-        style={dark}
-      >
-        {code.trim()}
-      </SyntaxHighlighter>
+      <Accordion>
+        {mdxTags.title && (
+          <AccordionSummary
+            sx={{
+              backgroundColor: 'background.default',
+              margin: 0,
+            }}
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography variant={'body1'} sx={{ margin: 0 }}>
+              {mdxTags.title}
+            </Typography>
+          </AccordionSummary>
+        )}
+        <AccordionDetails>
+          <SyntaxHighlighter
+            wrapLongLines={true}
+            language={mdxTags.language}
+            style={dark}
+            customStyle={{ margin: 0, borderRadius: '5px' }}
+          >
+            {code.trim()}
+          </SyntaxHighlighter>
+        </AccordionDetails>
+      </Accordion>
     </Box>
   );
 };
--- components/MDXComponents/utils.ts
+++ components/MDXComponents/utils.ts
@@ -1,10 +1,29 @@
-export const extractMDXLanguage = (langClassName: string) => {
-  console.log('received lang string: ', langClassName);
-  const langArr = (langClassName || '').split('-');
+export const extractMDXTags = (tagString: string) => {
+  let lan = 'typescript';
+  let title = null;
+  let expanded = true;
 
-  if (langArr.length > 1) {
-    return langArr[1];
+  const langArrByColon = (tagString || '').split(':');
+
+  if (langArrByColon.length > 0) {
+    const langArr = langArrByColon[0].split('-');
+
+    if (langArr.length > 1) {
+      lan = langArr[1];
+    }
   }
 
-  return 'javascript';
+  if (langArrByColon.length > 1) {
+    title = langArrByColon[1];
+  }
+
+  if (langArrByColon.length > 2) {
+    expanded = langArrByColon[2] !== 'collapsed';
+  }
+
+  return {
+    language: lan,
+    title,
+    expanded,
+  };
 };
